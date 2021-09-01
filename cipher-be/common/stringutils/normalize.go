edb49f62aa2093db2811309afc60a00b2f140d0b
BeRay31 <beray.prof.if@gmail.com>
2021-09-01 08:51:02 +0700

    feat: update normalize utils

--- cipher-be/common/stringutils/normalize.go
+++ cipher-be/common/stringutils/normalize.go
@@ -5,16 +5,12 @@ import "strings"
 // Normalize normalizes a string to 26 alphabets only, all uppercase [A-Z]
 func Normalize(s string) string {
 	// set uppercase
-	s = strings.ToUpper(s)
-
-	// remove punctuation
-	s = strings.ReplaceAll(s, " ", "")
-	s = strings.ReplaceAll(s, ",", "")
-	s = strings.ReplaceAll(s, ".", "")
-	s = strings.ReplaceAll(s, ":", "")
-	s = strings.ReplaceAll(s, ";", "")
-	s = strings.ReplaceAll(s, "\"", "")
-	s = strings.ReplaceAll(s, "'", "")
-
-	return s
+	normalizedChar := []rune{}
+	for _, char := range s {
+		charBase := GetCharBase(char)
+		if charBase != -1 {
+			normalizedChar = append(normalizedChar, char)
+		}
+	}
+	return strings.ToUpper(string(normalizedChar))
 }
