56cb7b5e63f458be6d24fc2baa0255bfaf0ee740
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2021-09-06 12:48:51 +0700

    feat: to formdata

--- cipher-be/handlers/vigenere/extended/encrypt.go
+++ cipher-be/handlers/vigenere/extended/encrypt.go
@@ -1,28 +1,32 @@
 package extended
 
 import (
-	"encoding/base64"
+	"bytes"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/mkamadeus/cipher/cipher/vigenere/extended"
-	"github.com/mkamadeus/cipher/models"
 )
 
 func Encrypt(c echo.Context) error {
-	body := new(models.VigenereExtendedRequest)
-	err := c.Bind(body)
+	content, err := c.FormFile("content")
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, err)
 	}
+	key := c.FormValue("key")
 
-	encrypted := extended.Encrypt([]byte(body.Content), []byte(body.Key))
-	encryptedBase64 := base64.URLEncoding.EncodeToString(encrypted)
-	payload := &models.VigenereExtendedResponse{
-		BaseResponse: models.BaseResponse{
-			Content: encryptedBase64,
-		},
-		Key: body.Key,
+	contentSrc, err := content.Open()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
 	}
-	return c.JSON(http.StatusOK, payload)
+	defer contentSrc.Close()
+
+	contentBuffer := bytes.NewBuffer(nil)
+	if _, err := io.Copy(contentBuffer, contentSrc); err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	encrypted := extended.Encrypt(contentBuffer.Bytes(), []byte(key))
+	return c.Blob(http.StatusOK, "application/octet-stream", encrypted)
 }
